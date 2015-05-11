7a20b8e8317f6423dd7baa37a69a6e6463f6d996
Tim Mason <millionsofbillions@gmail.com>
2015-05-10 21:45:40 -0500

    filters and sums

--- VITEK-data_analysis.py
+++ VITEK-data_analysis.py
@@ -16,7 +16,7 @@ import percentchange as pc
 path=os.path.dirname(__file__)
 dataDir=os.path.join(path,'Data')
 
-cardRun = collections.namedtuple('CardRun', 'data OpticSN opticMod condition')
+cardRun = collections.namedtuple('CardRun', 'data runNum Optic TXmod condition')
 
 dataSet=[]
 for condition in os.listdir(dataDir):     
@@ -27,32 +27,56 @@ for condition in os.listdir(dataDir):
     for optic in os.listdir(conditionPath):
         #print(optic)
         opticDir=os.path.join(conditionPath,optic)
-        
+        TX1count = 0;
+        TX3count = 0
         for testRun in os.listdir(opticDir):
             #print(testRun)
             #add this to database
-            opticMod=testRun.split()[0]
-                            
+            TXmod=testRun.split()[0]
+            if TXmod == 'TX1':
+                TX1count += 1
+                runNum = TX1count
+            elif TXmod == 'TX3':
+                TX3count += 1
+                runNum = TX3count  
             with open(os.path.join(opticDir,testRun), 'r') as file:
                 #load data into numpy array
-                dataSet.append(cardRun(np.loadtxt(file)[:,1:],optic,opticMod,dir))
+                dataSet.append(cardRun(np.loadtxt(file)[:,1:],runNum, optic,TXmod,condition))
                 file.close() 
 
 
 sumAll=np.zeros(64,)
-for i in range(0,12):
-    data=dataSet[i]
-    print(data[1:])
-    #mp.multiPlot(data[0])
-    pcnt=pc.calcPC(data[0])
-    print(pc.calcSumPC(pcnt,7).shape)
-    sumAll += pc.calcSumPC(pcnt,7)
-    #plt.figure()
-    #plot sum percent change for first 7 reads
-    #mp.pcntChangeLayout(pc.calcSumPC(pcnt,7),30)
-    #plot maximum percent change
-    #plt.figure()
-    #mp.pcntChangeLayout(pcnt.max(0),8)
-mp.pcntChangeLayout(sumAll,430)    
+
+filteredSets = []
+for mod in ['TX1', 'TX3']:
+    #filteredSets.append([mod + ' only:           ',[j for j in range(0,len(dataSet)) if dataSet[j].TXmod == mod and dataSet[j].condition == 'Data_Unsealed']])
+    #filteredSets.append([mod + ' only:           ',[j for j in range(0,len(dataSet)) if dataSet[j].TXmod == mod and dataSet[j].condition == 'Data_Unsealed']])
+    filteredSets.append([mod + ' only:           ',[j for j in range(0,len(dataSet)) if dataSet[j].TXmod == mod and dataSet[j].condition == 'Data_Unfilled' and dataSet[j].runNum < 6 ]])
+    filteredSets.append([mod + ' only:           ',[j for j in range(0,len(dataSet)) if dataSet[j].TXmod == mod and dataSet[j].condition == 'Data_Unsealed' and dataSet[j].runNum < 6 ]])
+    filteredSets.append([mod + ' only:           ',[j for j in range(0,len(dataSet)) if dataSet[j].TXmod == mod and dataSet[j].condition == 'Data_HeaterOff' and dataSet[j].runNum < 6 ]])
+    filteredSets.append([mod + ' only:           ',[j for j in range(0,len(dataSet)) if dataSet[j].TXmod == mod and dataSet[j].condition == 'Data_Normal' and dataSet[j].runNum < 6 ]])
+
+   
+for j in filteredSets:
+    for i in j[1]:   #loop through the data in the Jth filtered set
+        data=dataSet[i]
+        print(data[1:])
+        #mp.multiPlot(data[0])
+        pcnt=pc.calcPC(data[0])
+        print(np.minimum(pc.calcSumPC(pcnt,7),30*np.ones(64)))
+        sumAll += np.minimum(pc.calcSumPC(pcnt,7),30*np.ones(64))
+        
+        #plt.figure()
+        #plot sum percent change for first 7 reads
+        #mp.pcntChangeLayout(pc.calcSumPC(pcnt,7),30)
+        #plot maximum percent change
+        #plt.figure()
+        #mp.pcntChangeLayout(pcnt.max(0),8)
+    plt.figure()
+    print(len(j[1]))
+    average=sumAll/len(j[1])
+    print(sumAll)
+    print(average)
+    mp.pcntChangeLayout(average,40)    
 
 
